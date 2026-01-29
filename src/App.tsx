b8b15d1f9d1a9ef437ec6d98c17f33a045e0b420
Markus Dunkel <mrks.dunkel@gmail.com>
2026-01-29 13:37:48 +0100

    baseName added to react router

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { NotFoundPage } from './pages/not-found';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/oberhof-website">
       <Routes>
         <Route element={<HomeLayout />}>
           <Route index element={<HomePage />} />

7b14bc9dd7c18f675feaada093b24d55137f077d
Markus Dunkel <mrks.dunkel@gmail.com>
2026-01-29 13:00:43 +0100

    corrections

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "node:path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 export default defineConfig({
+  base: '/oberhof-website/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
